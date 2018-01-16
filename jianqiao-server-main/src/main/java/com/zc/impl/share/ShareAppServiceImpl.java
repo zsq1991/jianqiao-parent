054cdaaa8b9a14cc0cc2f5fbe6ea744cc7464850
zhaojunbiao <1754031481@qq.com>
2018-01-16 15:03:54 +0800

    web分享页面提交controller service

---
+++ jianqiao-server-main/src/main/java/com/zc/impl/share/ShareAppServiceImpl.java
@@ -0,0 +1,80 @@
+package com.zc.impl.share;
+
+import com.zc.main.service.share.ShareAppService;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @package : com.zc.impl.share
+ * @progect : jianqiao-parent
+ * @Description :
+ * @Created by :ZhaoJunBiao
+ * @Creation Date ：2018年01月16日15:01
+ */
+public class ShareAppServiceImpl implements ShareAppService {
+    @Override
+    public Map<String, Object> getConsultationnow(String id) {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getConsultation(String s) {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getConsultationTop(String id) {
+        return null;
+    }
+
+    @Override
+    public List<Map<String, Object>> getConsultationList(String s) {
+        return null;
+    }
+
+    @Override
+    public List<Map<String, Object>> getFTDetailList(String s) {
+        return null;
+    }
+
+    @Override
+    public List<Map<String, Object>> getConsultationDetail(String s) {
+        return null;
+    }
+
+    @Override
+    public List<Map<String, Object>> getreplyDetail(String commentid) {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getFTPLNum(String s) {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getHelpAuthor(String id) {
+        return null;
+    }
+
+    @Override
+    public List<Map<String, Object>> getImageList(String id) {
+        return null;
+    }
+
+    @Override
+    public List<Map<String, Object>> getHelpAuthorIdList(String id) {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getAuthorUserList(String bid) {
+        return null;
+    }
+
+    @Override
+    public List<Map<String, Object>> getAuthorList(String bid) {
+        return null;
+    }
+}
