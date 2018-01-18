b14261f806acc83b902a787ea2f9971eeab468d9
xt <1165790354@qq.com>
2018-01-18 15:41:49 +0800

    "消息信息全部获取接口 "

--- jianqiao-facade-member/src/main/java/com/zc/main/service/membermsg/MemberMsgService.java
+++ jianqiao-facade-member/src/main/java/com/zc/main/service/membermsg/MemberMsgService.java
@@ -21,4 +21,21 @@ public interface MemberMsgService{
 	 * @return
 	 */
 	public Result getMemberMsgReadInform(Member members);
+	/**
+	 * 阅读通知信息
+	 * @param members
+	 * @param msgId 只有当type为1时，msgId时必须填写，MemberMsg的id
+	 * @param type 1审核通知，2赞通知，3评论通知
+	 * @return
+	 */
+	public Result getReadInformList(Member members,Long msgId,Integer type);
+	/**
+	 * 审核通知列表
+	 * @param member
+	 * @param page
+	 * @param rows
+	 * @param type 1审核通知，2赞通知，3评论通知
+	 * @return
+	 */
+	public Result getInformList(Member member,Integer page,Integer rows,Integer type);
 }
