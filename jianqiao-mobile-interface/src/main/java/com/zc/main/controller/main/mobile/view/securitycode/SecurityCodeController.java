0222baa2dcb7054ff68c5e0a14146cde01208395
tenghui <tenghui8080@gmail.com>
2018-01-18 15:50:48 +0800

    验证码

--- jianqiao-mobile-interface/src/main/java/com/zc/main/controller/main/mobile/view/securitycode/SecurityCodeController.java
+++ jianqiao-mobile-interface/src/main/java/com/zc/main/controller/main/mobile/view/securitycode/SecurityCodeController.java
@@ -40,4 +40,39 @@ public class SecurityCodeController {
         Result result = securityCodeService.sendCodeLoginByPhone(phone,codeType);
         return result;
     }
+
+    /**
+     * @description ：检验验证码
+     * @Created by  : tenghui
+     * @Creation Date ： 2018/1/18 15:43
+     * @version : 1.0.0
+     * @param phone
+     * @param code
+     * @return
+     */
+    @RequestMapping(value="registercheck",method=RequestMethod.POST)
+    @ResponseBody
+    public Result checkCodeRegisterByPhone(@RequestParam(value="phone") String phone,@RequestParam(value="code") String code){
+        String codeType="JQ2017613";
+        Result result = securityCodeService.checkMessageCode(phone,code,codeType);
+        return result;
+
+    }
+    /**
+     * @description ：用户注册，获取验证码
+     * @Created by  : tenghui
+     * @Creation Date ： 2018/1/18 15:49
+     * @version : 1.0.0
+     * @param phone
+     * @return
+     */
+    @RequestMapping(value="registersend",method=RequestMethod.POST)
+    @ResponseBody
+    public Result sendCodeRegisterByPhone(@RequestParam(value="phone") String phone){
+        String codeType="JQ2017613";
+        Result result = securityCodeService.sendCodeRegisterByPhone(phone,codeType);
+        return result;
+    }
+
+
 }
