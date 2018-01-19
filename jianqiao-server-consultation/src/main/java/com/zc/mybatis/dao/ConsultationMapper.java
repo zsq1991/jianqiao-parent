bf8170bfd92aa3e63045498519952aeb53fa9ed5
xt <1165790354@qq.com>
2018-01-19 11:58:13 +0800

    修改发布资讯信息 <访谈,口述，求助、分享>

--- jianqiao-server-consultation/src/main/java/com/zc/mybatis/dao/ConsultationMapper.java
+++ jianqiao-server-consultation/src/main/java/com/zc/mybatis/dao/ConsultationMapper.java
@@ -33,9 +33,9 @@ public interface ConsultationMapper extends BasicMapper<Consultation> {
 	Consultation findOne(Long id);
 	/**
 	 * 在修改编辑资讯的时候删除审核失败通知信息
-	 * @param conId
-	 * @param mId
-	 * @param msgType
+	 * @param conId  资讯id
+	 * @param mId  y用户id
+	 * @param msgType  消息类型
 	 */
 	public void deleteMemberMsgByConId(@org.apache.ibatis.annotations.Param("conId")Long conId,@org.apache.ibatis.annotations.Param("mId") Long mId, @org.apache.ibatis.annotations.Param("msgType")Integer msgType);
 	/**
