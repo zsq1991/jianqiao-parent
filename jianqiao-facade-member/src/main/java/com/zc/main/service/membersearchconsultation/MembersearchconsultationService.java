f39b18373d2b67096fc48e9e9d9df765c6fee20f
wangxueyang <wxueyanghj@163.com>
2018-01-22 10:12:10 +0800

    丰富注释

--- jianqiao-facade-member/src/main/java/com/zc/main/service/membersearchconsultation/MembersearchconsultationService.java
+++ jianqiao-facade-member/src/main/java/com/zc/main/service/membersearchconsultation/MembersearchconsultationService.java
@@ -16,10 +16,48 @@ import java.util.Map;
  * @Creation Date ：2018年01月17日17:08
  */
 public interface MembersearchconsultationService {
-    List<Map<String,Object>> findSearchKeywordByMember(Map<String, Object> map);
 
+    /**
+     * * @author:  wangxueyang[wxueyanghj@163.com]
+     *
+     * @param map
+     * @return
+     * @create: 2018/1/22 9:37
+     * @desc: 获取用户历史检索关键词
+     * @version 1.0.0
+     */
+    List<Map<String, Object>> findSearchKeywordByMember(Map<String, Object> map);
+
+    /**
+     * * @author:  wangxueyang[wxueyanghj@163.com]
+     *
+     * @param map
+     * @return
+     * @create: 2018/1/22 9:37
+     * @desc: 获取资讯信息
+     * @version 1.0.0
+     */
     Integer getSearchConsultationByInfo(HashMap<String, Object> map);
+
+    /**
+     * * @author:  wangxueyang[wxueyanghj@163.com]
+     *
+     * @param id 用户id
+     * @param info 关键词
+     * @return
+     * @create: 2018/1/22 9:38
+     * @desc: 保存用户检索关键词
+     * @version 1.0.0
+     */
     Result saveMemberSearchConsultation(Long id, String info);
 
+    /**
+     * * @author:  wangxueyang[wxueyanghj@163.com]
+     * @create: 2018/1/22 9:39
+     * @desc: 删除关键词
+     * @version 1.0.0
+     * @param member
+     * @return
+     */
     Result deleteKeys(Member member);
 }
